993fc0897dbc645e21113510dabc772bb310e4f0
Phokoos <hulkovskij@gmail.com>
2023-03-10 16:18:13 +0200

    Finished 1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,27 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+const newGalleryEl = galleryItems.map(({preview, original, description}) =>
+	`<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`).join("");
+
+galleryEl.innerHTML = newGalleryEl;
+
+new SimpleLightbox('.gallery a', {
+	captions: true,
+	captionsData: 'alt',
+	captionPosition: 'bottom',
+	captionDelay: 250,
+});
+
+
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,37 @@
+import Player from '@vimeo/player';
+
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe);
+
+const onPlay = () => {
+	console.log("Video play");
+} 
+
+// player.on("timeupdate", (target) => {
+// 	console.log(target);
+// })
+
+const check = () => {
+	player.on("timeupdate", (target) => {
+		console.log(target.seconds);
+	})
+
+throttle(check, 500);
+
+player.setCurrentTime(30).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+			 console.log("Ohh, false time");
+            break;
+        default:
+            // some other error occurred
+			 console.log("Ohh, false value");
+            break;
+    }
+});
+
 
