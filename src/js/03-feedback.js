ffffbb468b88fd55b803d1b49596b139f4894512
Phokoos <hulkovskij@gmail.com>
2023-03-10 18:07:02 +0200

    Added some functionals to 3-rd task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import throttle from 'lodash.throttle';	
+
+const elInputForm = document.querySelector(".feedback-form");
+const elInputEmail = document.querySelector("[name=email]");
+const elInputMessage = document.querySelector("[name=message]");
+const btnSubmit = document.querySelector("[type=submit]");
+console.log(btnSubmit);
+
+const DATAS_STORAGE_KEY = 'feedback-form-state';
+let formDates = {
+	email: "",
+	message: ""
+}
+
+try {
+	if (JSON.parse(localStorage.getItem(DATAS_STORAGE_KEY)) !== null) {
+		formDates = JSON.parse(localStorage.getItem(DATAS_STORAGE_KEY));
+	}
+} catch (error) {
+	console.log(error);
+}
+
+elInputEmail.value = formDates.email;
+elInputMessage.value = formDates.message;
+
+elInputForm.addEventListener("input",  throttle(event => writeFormValueToStorage(event), 1000));
+
+function writeFormValueToStorage(event) {
+		formDates[event.target.name] = event.target.value;
+	
+	localStorage.setItem(DATAS_STORAGE_KEY, JSON.stringify(formDates));
+}
 
